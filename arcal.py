db908251fced5e81f219edc6c7d2a991933584b6
UsaidAkmal <usaidakmallinorobbanni@gmail.com>
2025-06-22 08:18:56 +0700

    done

--- arcal.py
+++ arcal.py
@@ -1,54 +1,66 @@
 while True:
     try:
+        print(f'''
+
+                    __
+ ___ ____________ _/ /
+/ _ `/ __/ __/ _ `/ / 
+\_,_/_/  \__/\_,_/_/  
+arithmetic-calculator
+              
+SIMPLE CALCULATOR WITH PYTHON :) start with math and py
+
+''')
+
         num1 = input(f"[+] Masukan angka pertama: ")
         operator = input(f'''
 
-        [+] Masukan operator yang anda inginkan 
-        | + | - | * | / | % | // | ** |
-        pilih salah satunya! 
+[+] Masukan operator yang anda inginkan
+                          
+|  +  |  -  |  *  |  /  |  %  |  //  |  **  |
 
-        ''')
-        num2 = input(f"[+] Masukan angka kedua: ")
+PILIH SALAH SATU OPERATOR!: ''')
+        num2 = input(f"\n[+] Masukan angka kedua: ")
 
         num1_int = int(num1)
         num2_int = int(num2)
 
         if operator == "+":
-            print(f"hasilnya adalah", num1_int+num2_int)
+            print(f"\nhasilnya adalah", num1_int+num2_int)
             break
 
         elif operator == "-":
-            print(f"hasilnya adalah", num1_int-num2_int)
+            print(f"\nhasilnya adalah", num1_int-num2_int)
             break
 
         elif operator == "*":
-            print(f"hasilnya adalah", num1_int*num2_int)
+            print(f"\nhasilnya adalah", num1_int*num2_int)
             break
 
         elif operator == "/":
-            print(f"hasilnya adalah", num1_int/num2_int)
+            print(f"\nhasilnya adalah", num1_int/num2_int)
             break
 
         elif operator == "%":
-            print(f"hasilnya adalah", num1_int%num2_int)
+            print(f"\nhasilnya adalah", num1_int%num2_int)
             break
 
         elif operator == "//":
-            print(f"hasilnya adalah", num1_int//num2_int)
+            print(f"\nhasilnya adalah", num1_int//num2_int)
             break
 
         elif operator == "**":
-            print(f"hasilnya adalah", num1_int**num2_int)
+            print(f"\nhasilnya adalah", num1_int**num2_int)
             break
 
         else:
-            print(f"Input yang kamu masukan salah, berikan input yang benar")
+            print(f"\nInput yang kamu masukan salah, berikan input yang benar")
             continue
 
     except ValueError:
-        print(f"berikan inputan dengan format yang sesuai")
-        continue
+        print(f"\nberikan inputan dengan format yang sesuai")
+        break
 
     except ZeroDivisionError:
-        print(f"Pembagian dengan pembagi nol tidak di izinkan.")
-        continue
+        print(f"\nPembagian dengan pembagi nol tidak di izinkan.")
+        break
